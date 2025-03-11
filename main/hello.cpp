d592547c71cdda30ca7f6c2be9e16db685c65ae0
Dhanush S <clgdhanush232@gmail.com>
2025-03-11 15:54:38 +0530

    added a new line2

--- main/hello.cpp
+++ main/hello.cpp
@@ -8,6 +8,9 @@ int main(void) {
   cout << "I have successfully built and run this  \n";
   cout << " I have edited this file, for lab 8"; 
   cout << " I have also edited this line lol"; 
+  cout << "This is to test if webhooks automatically runs the build command"; 
+  cout << "ONE MORE CHANGE"; 
+  
   
   return 0;
 }
