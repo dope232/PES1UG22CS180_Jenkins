24066970ef799effecdffd18b6ae77527b41b40b
Dhanush S <clgdhanush232@gmail.com>
2025-03-11 15:24:03 +0530

    Describe this commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,8 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
+  cout << " I have edited this file, for lab 8"; 
+  cout << " I have also edited this line lol"; 
+  
   return 0;
 }
